f60483a0c6faf060a767106cceb819a3bbe9331f
sm1ck <45939749+sm1ck@users.noreply.github.com>
2021-02-17 00:30:36 +0500

    Fix cron bug

--- peers.sh
+++ peers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd
-wget -O ~/peers.txt https://raw.githubusercontent.com/MinaProtocol/mina/encore-peers/automation/terraform/testnets/testworld/peers.txt
-for i in $(cat peers.txt); do docker exec -it mina coda advanced add-peers $i; done
-check=$(docker exec -it mina coda client status | grep 'Sync status')
+wget -O ~/peers.txt https://raw.githubusercontent.com/MinaProtocol/mina/encore-peers/automation/terraform/testnets/encore/peers.txt
+for i in $(cat peers.txt); do docker exec mina coda advanced add-peers $i; done
+check=$(docker exec mina coda client status | grep 'Sync status')
 if [[ $check == *"Offline"* ]] || [[ $check == *"Connecting"* ]]; then
   docker restart mina
 fi
