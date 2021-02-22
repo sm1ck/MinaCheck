a472fe33de1f38f94a320c37b75c45480c503ee5
sm1ck <45939749+sm1ck@users.noreply.github.com>
2021-02-22 15:03:37 +0500

    Catch some errors

--- checkheight.sh
+++ checkheight.sh
@@ -3,7 +3,15 @@ curl -s 'https://api.minaexplorer.com/summary' > blocklength.txt
 VAR1=$(docker exec mina coda client status  | grep 'Block height' | grep -o -E '[0-9]+' | tr -cd '0-9')
 VAR2=$(cat blocklength.txt | grep 'blockchainLength' | grep -o -E '[0-9]+' | tail -1 | tr -cd '0-9')
 rm -f blocklength.txt
+re='^[0-9]+$'
+VAR1='t'
+if ! [[ $VAR1 =~ $re ]] || ! [[ $VAR2 =~ $re ]] ; then
+   NOW=$(date +"%d-%m-%Y %H:%M:%S")
+   echo "[$NOW] Error: not a number" >> crash.log
+   echo "Error: not a number" >&2; exit 1
+fi
 CALC=$((VAR2-VAR1)) # MinaExplorer - Your_Node, should be 0/1/2
+re='^[0-9]+$'
 if [ "$CALC" -le 2 ]; then
         echo $VAR1
         echo $VAR2
